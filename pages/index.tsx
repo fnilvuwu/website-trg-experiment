109a354e7783f3403a834c9f818fe07b47d8713c
fnilvuwu <mamatmks45@gmail.com>
2025-01-10 17:19:51 +0800

    modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { Typewriter } from '../components/typewriter'
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white flex flex-col">
       <NavBar />
-      <div className="relative h-screen pt-16 pb-16 px-4 flex items-center justify-center">
+      <div className="flex-grow relative pt-16 pb-16 px-4 flex items-center justify-center">
         <NeuralNetwork />
         <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center text-sm font-medium text-black/70">
           <Typewriter text="Data Science and Artificial Intelligence (DSAI) Laboratory" />
